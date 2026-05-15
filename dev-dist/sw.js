4fd91fb72bc033ff823364047dcf72690207d4ef
margarytalinevych <margaryta.linev@gmail.com>
2026-05-15 16:27:28 +0300

    fix deploy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-709a0e9a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.biep5p2j99c"
+    "revision": "0.554mo5gg53k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,15 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend' // Це треба додати (див. нижче)
-
+const isProd = import.meta.env.PROD;
+const basePrefix = '/moodflow-react';
 i18n
   .use(Backend) // Це дозволяє вантажити файли з папки public
   .use(initReactI18next)
   .init({
     backend: {
       // Шлях до твоїх перенесених файлів
-      loadPath: '/locales/{{lng}}.json', 
+      loadPath: `${basePrefix}/locales/{{lng}}.json`,
     },
     lng: localStorage.getItem('moodflow-lang') || 'uk',
     fallbackLng: 'uk',
